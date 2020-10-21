dc5fe66c007eb6aa6d59c5f9c91d01c75daaa114
Amarpreet Singh <ampit.xd@gmail.com>
2020-10-21 01:08:04 -0600

    Updated Reg Exp Server.js
    Ids which start from a character respond with a route not found error as they do not pass the reg exp match test.

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } =
 const server = http.createServer((req, res) => {
     if(req.url === '/api/products' && req.method === 'GET') {
         getProducts(req, res)
-    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
+    } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'GET') {
         const id = req.url.split('/')[3]
         getProduct(req, res, id)
     } else if(req.url === '/api/products' && req.method === 'POST') {
         createProduct(req, res)
-    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+    } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'PUT') {
         const id = req.url.split('/')[3]
         updateProduct(req, res, id)
-    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+    } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'DELETE') {
         const id = req.url.split('/')[3]
         deleteProduct(req, res, id)
     } else {
